76237d155761f81898f2bd085f4c56727f508c46
thaibacuong <thaicuong862003@gmail.com>
2024-04-15 16:55:58 +0700

    code connectDB

--- src/connectDB/ConnectDB.java
+++ src/connectDB/ConnectDB.java
@@ -1,5 +1,37 @@
 package connectDB;
 
-public class ConnectDB {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectDB {
+	public static Connection con = null;
+	private static ConnectDB instance = new ConnectDB();
+	public static ConnectDB getInstance() {
+		return instance;
+	}
+	public void connect() {
+		String url = "jdbc:sqlserver://localhost:1433;databasename=QLVRP";
+		String user = "sa";
+		String password="123456";
+		try {
+			con = DriverManager.getConnection(url,user,password);
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	public static Connection getConnection() {
+		return con;
+	}
+	public void disconnect() {
+		if(con !=null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+	}
 }
