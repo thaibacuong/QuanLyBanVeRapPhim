ce87cca48883796a1c325bb01969f13749599e5e
SEVENHY <107412465+SEVENHY@users.noreply.github.com>
2024-04-22 09:38:52 +0700

    change DatVe_UI

--- src/gui/DatVe_UI.java
+++ src/gui/DatVe_UI.java
@@ -6,7 +6,7 @@ public class DatVe_UI extends JFrame{
 	public DatVe_UI() {
 		
 		
-		setTitle("Phim");
+		setTitle("Đặt vé");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setSize(500, 400);
--- src/gui/TrangChu.java
+++ src/gui/TrangChu.java
@@ -1,5 +1,18 @@
 package gui;
 
-public class TrangChu {
+import javax.swing.JFrame;
 
+public class TrangChu extends JFrame{
+	public TrangChu() {
+		setTitle("Trang chủ");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(500, 400);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+	}
+	public static void main(String[] args) {
+		new TrangChu();
+	}
 }
